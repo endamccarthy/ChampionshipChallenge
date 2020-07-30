87c544e2bb6c2818ccc0df24899d24728d62735f
Enda McCarthy <enda.mccarthy@outlook.com>
2020-07-30 16:10:46 +0100

    removed leading / from static links in html

--- championshipchallenge/settings.py
+++ championshipchallenge/settings.py
@@ -307,7 +307,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # if not DEVELOPMENT_MODE:
 #   # look to s3 buckets for all static files including js and css
 #   STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # ######################################################################################## #
 
