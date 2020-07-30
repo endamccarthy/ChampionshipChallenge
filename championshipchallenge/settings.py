850bf5ef50d7853797fea81d03d2ac61902bf74f
Enda McCarthy <enda.mccarthy@outlook.com>
2020-07-30 16:21:44 +0100

    ops

--- championshipchallenge/settings.py
+++ championshipchallenge/settings.py
@@ -304,9 +304,9 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# if not DEVELOPMENT_MODE:
-#   # look to s3 buckets for all static files including js and css
-#   STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+if not DEVELOPMENT_MODE:
+  # look to s3 buckets for all static files including js and css
+  STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # ######################################################################################## #
 
