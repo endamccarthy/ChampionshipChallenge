c0ff816b43409153145db0f9d7eeeace549c0e7c
Enda McCarthy <enda.mccarthy@outlook.com>
2020-07-31 10:27:09 +0100

    set up connection to aws db

--- championshipchallenge/settings.py
+++ championshipchallenge/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import dj_database_url
 import os
 import django_heroku
 
@@ -138,6 +139,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #     'default': {
